6440b36fcfe03447c34471956670036018e98984
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-05-29 13:54:29 +0200

    Set eventListener to the buttons to render on the screen

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-(function(){
+(function() {
 
-    const screen = document.querySelector('.screen');
-    const buttons = document.querySelectorAll('.btn');
-    const clear = document.querySelector('.btn-clear');
-    const equal = document.querySelector('.btn-equal');
-})
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let clear = document.querySelector('.btn-clear');
+    let equal = document.querySelector('.btn-equal');
+
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function(e) {
+            let value = e.target.dataset.num;
+            screen.value += value;
+        })
+    })
+})();
