72c677677713887d79103bd83086b7e3ef8151ee
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-05-29 13:47:34 +0200

    Select all of my buttons

---
+++ app.js
@@ -0,0 +1,7 @@
+(function(){
+
+    const screen = document.querySelector('.screen');
+    const buttons = document.querySelectorAll('.btn');
+    const clear = document.querySelector('.btn-clear');
+    const equal = document.querySelector('.btn-equal');
+})
