66cc747a0fdeffe97c4bcae34020e33c1319c640
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-05-29 16:38:01 +0200

    Finish the Dark and White mode

--- index.js
+++ index.js
@@ -1,41 +1,49 @@
 // funcionality
-const screen = document.querySelector('.screen');
-const buttons = Array.from(document.querySelectorAll('.btn'));
-const actionButtons = Array.from(document.querySelectorAll('btn-action'));
-const equal = document.querySelector('.equal');
-const clear = document.querySelector('.clear');
-const back = document.querySelector('.back');
+const screen = document.querySelector(".screen");
+const buttons = Array.from(document.querySelectorAll(".btn"));
 
-buttons.map(buttons => {
-    buttons.addEventListener('click', e => {
-        
-        screen.value += buttons.textContent;
-        
-         
-       
-       
-    })
+const equal = document.querySelector(".equal");
+const clear = document.querySelector(".clear");
+const back = document.querySelector(".back");
+
+buttons.map((buttons) => {
+  buttons.addEventListener("click", (e) => {
+    screen.value += buttons.textContent;
+  });
 });
 
-
-equal.addEventListener('click', e => {
-    screen.value = eval(screen.value)
+equal.addEventListener("click", (e) => {
+  screen.value = eval(screen.value);
 });
 
-clear.addEventListener('click', e => {
-    screen.value = '';
+clear.addEventListener("click", (e) => {
+  screen.value = "";
 });
 
-back.addEventListener('click', e => {
-    screen.value = screen.value.slice(0, -1);
-})
+back.addEventListener("click", (e) => {
+  screen.value = screen.value.slice(0, -1);
+});
 
 // Dark - White modes
 
+const modeButton = document.querySelector(".btn-mode");
+const calculator = document.querySelector(".calculator");
 
+modeButton.addEventListener("click", (e) => {
+  if (modeButton.textContent === "WHITE") {
+    modeButton.textContent = "DARK";
+    calculator.style.background = "#fff";
+    screen.style.background = "#fff";
+    screen.style.color = "#000";
+    buttons.style.background = "white";
+  } else {
+    modeButton.textContent = "WHITE";
+    calculator.style.background = "#000";
+    screen.style.background = "#000";
+    screen.style.color = "#fff";
+    buttons.style.background = "#fff";
 
+  
 
-
-
-
-
+  }
+});
